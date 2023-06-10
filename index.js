141b3d14c9de926b8a28c470355e23ee8feb4e6f
Limon-0007 <limonahmed3023@gmail.com>
2023-06-10 14:38:38 +0600

    get users data

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     // classes related api
     app.get("/classes", async (req, res) => {
       const result = await classesCollection.find().toArray();
