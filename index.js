725e4c027c806345d83a968dc9f96ccaa62ce57c
Limon-0007 <limonahmed3023@gmail.com>
2023-06-11 17:56:41 +0600

    protected routes by jwt

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization; //need to get data from headers
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+
+  // bearer token
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s4mg6y0.mongodb.net/?retryWrites=true&w=majority`;
@@ -30,6 +52,14 @@ async function run() {
       .db("summerCampDB")
       .collection("instructors");
 
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "24h",
+      });
+      res.send({ token });
+    });
+
     // users related api
 
     app.post("/users", async (req, res) => {
@@ -43,7 +73,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyJwt, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -72,12 +102,12 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/users/:email",async (req, res) => {
-      const email = req.params.email
-      const query = {email: email}
-      const result = await usersCollection.find(query).toArray()
-      res.send(result)
-    })
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // classes related api
     app.get("/classes", async (req, res) => {
@@ -87,14 +117,11 @@ async function run() {
     });
 
     app.post("/classes", async (req, res) => {
-      const email = req.params.email;
-      const query = {email: email}
-      if(!email){
-        res.send([])
-      }
-      const result = await classesCollection.insertOne(query);
+      const newItem = req.body;
+      const result = await classesCollection.insertOne(newItem);
       res.send(result);
     });
+
 
     // instructors related api
     app.get("/instructors", async (req, res) => {
@@ -103,7 +130,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/instructors/:id", async (req, res) => {
+    app.get("/instructors/:id", verifyJwt, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await instructorsCollection.findOne(query);
